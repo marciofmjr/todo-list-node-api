a29f68feea36f94f4b4823d0877c25bd75ae7a12
Marcio Junior <marciofmjr@hotmail.com>
2022-03-07 14:29:47 -0300

    add and configure prisma

---
+++ prisma/migrations/20220307172851_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Item" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
+);
