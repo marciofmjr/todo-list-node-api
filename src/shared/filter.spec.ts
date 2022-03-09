4017acf74f33f6edeb6da40602d5c3ba169d8935
Marcio Junior <marciofmjr@hotmail.com>
2022-03-09 20:31:35 -0300

    create filter to list endpoints with params

---
+++ src/shared/filter.spec.ts
@@ -0,0 +1,96 @@
+// NODE_ENV=test npx jest src/shared/filter.spec.ts --verbose --detectOpenHandles --bail --forceExit --watchAll
+
+import filter from './filter'
+
+it('given empty params, should return as expected', async () => {
+  const params = filter()
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { createdAt: 'desc' }
+  })
+})
+
+it('given q=name:*house , should return as expected', async () => {
+  const params = filter({ q: 'name:*house' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { createdAt: 'desc' },
+    where: { name: { startsWith: 'house' } }
+  })
+})
+
+it('given q=name:house*,active:true , should return as expected', async () => {
+  const params = filter({ q: 'name:house*,active:true' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { createdAt: 'desc' },
+    where: { name: { endsWith: 'house' }, active: true }
+  })
+})
+
+it('given q=name:*house* , should return as expected', async () => {
+  const params = filter({ q: 'name:*house*' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { createdAt: 'desc' },
+    where: { name: { contains: 'house' } }
+  })
+})
+
+it('given page=1&limit=5, should return as expected', async () => {
+  const params = filter({ page: 1, limit: 5 })
+  expect(params).toEqual({
+    skip: 0,
+    take: 5,
+    orderBy: { createdAt: 'desc' }
+  })
+})
+
+it('given page=2&limit=5, should return as expected', async () => {
+  const params = filter({ page: 2, limit: 5 })
+  expect(params).toEqual({
+    skip: 5,
+    take: 5,
+    orderBy: { createdAt: 'desc' }
+  })
+})
+
+it('given page=3&limit=5, should return as expected', async () => {
+  const params = filter({ page: 3, limit: 5 })
+  expect(params).toEqual({
+    skip: 10,
+    take: 5,
+    orderBy: { createdAt: 'desc' }
+  })
+})
+
+it('given order=title, should return as expected', async () => {
+  const params = filter({ order: 'title' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { title: 'desc' }
+  })
+})
+
+it('given order=title:asc, should return as expected', async () => {
+  const params = filter({ order: 'title:asc' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { title: 'asc' }
+  })
+})
+
+it('given order=title:desc, should return as expected', async () => {
+  const params = filter({ order: 'title:desc' })
+  expect(params).toEqual({
+    skip: 0,
+    take: 50,
+    orderBy: { title: 'desc' }
+  })
+})
---
+++ src/shared/filter.ts
@@ -0,0 +1,79 @@
+interface Params {
+  q?: string
+  limit?: number
+  page?: number
+  order?: string
+}
+
+interface Filter {
+  take: number
+  skip: number
+  orderBy: object
+  where?: object
+  include?: object
+}
+
+export default (params?: Params): Filter => {
+  const filter: Filter = {
+    skip: 0,
+    take: 50,
+    orderBy: { createdAt: 'desc' }
+  }
+
+  if (!params) {
+    return filter
+  }
+
+  if (params.limit && params.limit > 0) {
+    filter.take = params.limit
+  }
+
+  if (params.page && params.page !== 0 && params.page !== 1) {
+    const index = +params.page - 1
+    let skip = 0
+    for (let i = 1; i <= index; i++) {
+      skip += filter.take
+    }
+    filter.skip = skip
+  }
+
+  if (params.q) {
+    filter.where = {}
+    const props = params.q.split(',')
+
+    for (const prop of props) {
+      const data = prop.split(':')
+      const name = data[0]
+      const value = data[1]
+
+      if (!name || !value) { continue }
+
+      if (value.substring(1, 0) === '*' && value.slice(-1) === '*') { // Like *marcio*
+        filter.where[name] = { contains: value.replace('*', '').replace('*', '') }
+      } else if (value.substring(1, 0) === '*') { // Like *marcio
+        filter.where[name] = { startsWith: value.replace('*', '') }
+      } else if (value.slice(-1) === '*') { // Like marcio*
+        filter.where[name] = { endsWith: value.replace('*', '') }
+      } else if (value === 'true') {
+        filter.where[name] = true
+      } else if (value === 'false') {
+        filter.where[name] = false
+      } else {
+        filter.where[name] = value
+      }
+    }
+  }
+
+  if (params.order) {
+    const order = params.order.split(':')
+    if (order.length === 1) {
+      filter.orderBy = {}
+      filter.orderBy[order[0]] = 'desc'
+    } else if (order.length === 2) {
+      filter.orderBy = {}
+      filter.orderBy[order[0]] = order[1].toLocaleLowerCase()
+    }
+  }
+
+  return filter
+}
